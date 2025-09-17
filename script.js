3896dd6db59758d33f07ba0c713b1fbd568f29f9
Beauty Kumari <117575930+beauty886@users.noreply.github.com>
2025-09-17 22:33:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
-function mincost(arr)
-{ 
-//write your code here
-// return the min cost
-  
+function mincost(arr) { 
+  let cost = 0;
+
+  while (arr.length > 1) {
+    // sort array to get two smallest ropes
+    arr.sort((a, b) => a - b);
+
+    // take two smallest
+    let first = arr[0];
+    let second = arr[1];
+
+    // merge cost
+    let merged = first + second;
+    cost += merged;
+
+    // remove first two and push merged
+    arr.splice(0, 2);
+    arr.push(merged);
+  }
+
+  return cost;
 }
 
-module.exports=mincost;
+module.exports = mincost;
